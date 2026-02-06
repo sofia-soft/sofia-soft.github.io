a72bb585b7e69e9d823bacc69e0da6d51f143fdb
Chavdar Stoilov <CVStoilov@yettel.bg>
2026-02-06 13:49:16 +0200

    Web optimization

--- js/burger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.burger');
-    const navList = document.querySelector('.nav_list');
-
-    burger.addEventListener('click', () => {
-        navList.classList.toggle('active');
-        burger.classList.toggle('active'); // показва X вместо burger
-    });
-});
--- js/javascript.js
+++ js/javascript.js
@@ -10,6 +10,18 @@ const navigator = {
     "contact": document.querySelector("#contact")
 };
 
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const navList = document.querySelector('.nav_list');
+
+    burger.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        burger.classList.toggle('active'); // показва X вместо burger
+    });
+});
+
+
+
 function scroll(event) {
     const clickedButton = event.target.className;
 
