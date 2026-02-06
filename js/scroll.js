55c83b11cdc6b95ec09832063a348feee350ead3
Chavdar Stoilov <CVStoilov@yettel.bg>
2026-02-06 10:27:40 +0200

    Update code

--- js/scroll.js
+++ js/scroll.js
@@ -1,22 +1,31 @@
 const nav = document.querySelectorAll(".nav_list li");
 const scrollUp = document.querySelector(".up");
-
+const more = document.querySelector(".hero_button_more");
 
 const navigator = {
     "home": document.querySelector(".header"),
     "services": document.querySelector("#services"),
     "how_work": document.querySelector("#how_work"),
     "for_us": document.querySelector("#about_us"),
-    "how_wor": document.querySelector("#how_work")
+    "contact": document.querySelector("#contact")
 };
 
 function scroll(event) {
     const clickedButton = event.target.className;
 
+    console.log(clickedButton);
+
     if (clickedButton === "up") {
         navigator['home'].scrollIntoView({
             behavior: "smooth"
         });
+        return;
+    }
+
+    if (clickedButton === "hero_button_more") {
+        navigator['for_us'].scrollIntoView({
+            behavior: "smooth"
+        })
     }
 
     navigator[clickedButton].scrollIntoView({
@@ -31,5 +40,9 @@ if (nav) {
 
 
 if (scrollUp) {
-    scrollUp.addEventListener("click", scroll)
+    scrollUp.addEventListener("click", scroll);
+}
+
+if (more) {
+    more.addEventListener("click", scroll);
 }
